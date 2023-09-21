fd69853e7ca26d3764bf0c592b784ed9383ba9c7
PavanKumarKolli <141021827+PavanKumarKolli@users.noreply.github.com>
2023-09-21 17:58:07 +0530

    second commit
    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Routers,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
@@ -6,22 +11,31 @@ import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  let userStorage = localStorage.getItem("user");
+  let UUU = localStorage.getItem("uuu");
 
-  console.log(userStorage);
+  console.log(UUU);
 
   return (
     <div className="App">
       <Routers>
         <div className="mainnn">
           <Routes>
-      <Route
+            <Route
               path="/"
               element={UUU ? <Navigate to="/home" /> : <Navigate to="/login" />}
             />
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/home"
+              element={UUU ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={UUU ? <Navigate to="/home" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={UUU ? <Navigate to="/home" /> : <Signup />}
+            />
           </Routes>
         </div>
       </Routers>
@@ -30,3 +44,4 @@ function App() {
 }
 
 export default App;
+
